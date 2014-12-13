577975895dbbe3279362d8a7df82ce9e529c5184
NashLegend <panzhihuipzh@gmail.com>
2014-12-13 15:47:09 +0800

    Draw Smoothly

--- app/src/main/java/com/example/smoothdraw/MainActivity.java
+++ app/src/main/java/com/example/smoothdraw/MainActivity.java
@@ -1,39 +1,99 @@
 package com.example.smoothdraw;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.widget.ImageView;
 
+public class MainActivity extends Activity implements OnTouchListener {
 
-public class MainActivity extends ActionBarActivity {
+    private ImageView view;
+    private Path path;
+    private Paint paint;
+    private Canvas canvas;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
+        view = (ImageView) findViewById(R.id.view);
+        view.setOnTouchListener(this);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                bitmap = Bitmap.createBitmap(getWindowWidth(),
+                        getWindowHeight(), Config.ARGB_8888);
+                canvas = new Canvas(bitmap);
+                paint = new Paint();
+                path = new Path();
+                path.moveTo(event.getX(), event.getY());
+                paint.setAntiAlias(true);
+                paint.setARGB(255, 0, 0, 255);
+                paint.setStrokeWidth(8);
+                paint.setStyle(Style.STROKE);
+                view.setImageBitmap(bitmap);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int size = event.getHistorySize();
+                for (int i = 0; i < size; i++) {
+                    path.lineTo(event.getHistoricalX(i), event.getHistoricalY(i));
+                }
+                path.lineTo(event.getX(), event.getY());
+                canvas.drawPath(path, paint);
+                view.invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                canvas.drawPath(path, paint);
+                view.invalidate();
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        clear();
+                    }
+                }, 3000);
+                break;
+            default:
+                break;
+        }
+
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void clear() {
+        if (canvas != null) {
+            Paint p = new Paint();
+            p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+            canvas.drawPaint(p);
+            p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
+            view.invalidate();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+    public int getWindowWidth() {
+        return getWindow().findViewById(Window.ID_ANDROID_CONTENT).getWidth();
+    }
+
+    public int getWindowHeight() {
+        return getWindow().findViewById(Window.ID_ANDROID_CONTENT).getHeight();
+    }
+
 }
